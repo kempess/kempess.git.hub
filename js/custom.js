c04a43f1b6d7b6e6bfae06390418d5cce5900239
Muhammad Rizky Maulana Putra Suhaemi <muhrmps123@gmail.com>
2021-01-26 02:17:36 +0700

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+/*!
+ * Item: Portofolio 
+ * Description: Portofolio Muhammad Rizky MPS
+ * Author/Developer: Exill
+ * Author/Developer URL: https://themeforest.net/user/exill
+ * Version: v1.0.0
+ * License: Themeforest Standard Licenses: https://themeforest.net/licenses
+ */
+!function(n){"use strict";n(document).ready(function(){}),n(window).on("load",function(){})}(jQuery);
